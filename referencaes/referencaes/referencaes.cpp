25844c2296b7c6442231d2c769ee5de268c3389a
Alumno <Alumno@PC35>
2019-11-07 11:31:39 -0600

    parametro referrencial

---
+++ referencaes/referencaes/referencaes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//prototipo de funciones
+void intercambio(int*, int*);
+int main()
+{
+	int a, b;
+	cout << "introduce el primer valor: ";
+	cin >> a;
+	cout << "introduce el segundo valor: ";
+	cin >> b;
+	cout << endl;
+	cout << " valor de a: " << a <<
+		"valor de b: " << b << endl;
+	intercambio(&a, &b);
+	cout << endl << "despues del intercambio:" <<
+		endl << endl;
+	cout << "el valor de a: " << a << "valor de b: "
+		<< b << endl;
+	system("pausa");
+	return 0;
+}
+void intercambio(int* x, int* y)
+{
+	int z;
+	z = *x;
+	*x = *y;
+	*y = z;
+}
